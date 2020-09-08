0d16f327d91a5c424ec07f04f2923c051d163e0a
Doug Johnson <dougvj@gmail.com>
2020-09-08 03:13:48 -0600

    Fix comments on vertex to remove misconception about clipping and point size

--- vertex.glsl
+++ vertex.glsl
@@ -13,8 +13,8 @@ void main() {
     //This just passes the position to the fragment shader
     position = in_position;
     //This is actually used by opengl to determine the fragements to render
-    //Note that it is a vec4 so the first 2 points are the output points, the
-    //thirds is the point size and the fouth clipping control.
+    //Note that it is a vec4, we have no z, and w is fixed to one, which is 
+    //used to scale the rest of the vector
     gl_Position = vec4(in_position.x, in_position.y, 0.0, 1.0);
     //We could also change the colors here, but we don't bother
     color = in_color;
